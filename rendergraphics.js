50f55588ce6dde01ae7e377a1fa946cf1cebbfc0
the-archer <simratsinghchhabra@gmail.com>
2015-10-09 23:47:26 -0700

    added rendergraphics partial

--- rendergraphics.js
+++ rendergraphics.js
@@ -1,53 +1,95 @@
-function rendergraphics_full(arr){
-	var body = document.getElementsByTagName("body")[0];
+
+    function rendergraphics_full(arr){
+    //alert("sirmaraer");
+    var body = document.getElementsByTagName("body")[0];
 
         // create elements <table> and a <tbody>
         var tbl     = document.createElement("table");
         var tblBody = document.createElement("tbody");
-	for(i=0; i<arr.length; i++)
-	{
-		var row = document.createElement("tr");
-		for(j=0; i<arr[i].length; j++)
-		{
-			 var cell = document.createElement("td"); 
-			 cell.setAttribute("id", i.toString()+","+j.toString());
-			 var img = document.createElement('img');
+    
+    for(i=0; i<arr.length; i++)
+    {
 
-    		if(arr[i][j].charAt(0)=='R')
-    		{
-    			img.src = "images/roadC.png";
-			}
-			else if(arr[i][j].charAt(0)=='P')
-			{
-				img.src = "images/house2.jpg";
-			}    		
-			else if(arr[i][j].charAt(0)=='G')
-			{
-				img.src = "images/Grass.png";
-			}
-			else if(arr[i][j].charAt(0)=='S')
-			{
-				img.src = "images/user.jpg";
-			}
-			else if(arr[i][j].charAt(0)=='D')
-			{
-				img.src = "images/icecream.jpg";
-			}
-			else if(arr[i][j].charAt(0) == 'T')
-			{
-				img.src = "images/roadC.png";
-			}
-			cell.appendChild(img);
+        var row = document.createElement("tr");
+        //alert(i);
+        for(j=0; j<arr[i].length; j++)
+        {
+            //alert(j);
+             var cell = document.createElement("td"); 
+             //cell.setAttribute("border", "none");
+             cell.setAttribute("id", i.toString()+","+j.toString());
+             var img = document.createElement('img');
+              //var cellText = document.createTextNode("cell is row "+j+", column "+i); 
+              //cell.appendChild(cellText);
+            if(arr[i][j].charAt(0)=='R')
+            {
+                img.src = "images/roadC.png";
+            }
+            else if(arr[i][j].charAt(0)=='P')
+            {
+                img.src = "images/house.jpg";
+            }           
+            else if(arr[i][j].charAt(0)=='G')
+            {
+                img.src = "images/Grass.jpg";
+            }
+            else if(arr[i][j].charAt(0)=='S')
+            {
+                img.src = "images/user.jpg";
+            }
+            else if(arr[i][j].charAt(0)=='D')
+            {
+                img.src = "images/icecream.jpg";
+            }
+            else if(arr[i][j].charAt(0) == 'T')
+            {
+                img.src = "images/roadC.png";
+            }
+            else if(arr[i][j].charAt(0) == 'C')
+            {
+                img.src = "images/cop.jpg";
+            }
+            cell.appendChild(img);
 
-			 row.appendChild(cell);
+             row.appendChild(cell);
 
-		}
+        }
 
-		tblBody.appendChild(row);
+        tblBody.appendChild(row);
 
-		tbl.appendChild(tblBody);
+       
+    
+
+
+    }
+
+     tbl.appendChild(tblBody);
         // put <table> in the <body>
-        body.appendChild(tbl);
+    body.appendChild(tbl);
+    tbl.setAttribute("border", "0");
+}
 
+function rendergraphics_partial(arr, i, j, oldi, oldj)
+{
+	//alert("here");
+	var cell = document.getElementById(i+","+j);
+	//alert(cell);
+	//alert("sir");
+	if (arr[i][j].charAt(0) == 'S')
+	{
+		//alert("here");
+		cell.getElementsByTagName('img')[0].src = "images/user.jpg";
+		
 	}
+
+	var cell = document.getElementById(oldi+","+oldj);
+	//alert(cell);
+	//alert("sir");
+	if (arr[oldi][oldj].charAt(0) == 'T')
+	{
+		//alert("here");
+		cell.getElementsByTagName('img')[0].src = "images/roadC.png";
+		
+	}
+
 }
